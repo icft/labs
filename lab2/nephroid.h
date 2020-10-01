f0846310f7568a1a4ae1b9291e477d3c9f52a034
ic_t <40525571+iconiq@users.noreply.github.com>
2020-10-01 16:22:47 +0300

    Update nephroid.h

--- lab2/nephroid.h
+++ lab2/nephroid.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <string>
+#include <stdexcept>
 
 class nephroid
 {
 private:
     double r;
 public:
+    static constexpr double ABS_ERROR = 0.000000000001;
     nephroid() {
         r = 0;
     }
@@ -16,9 +18,8 @@ public:
     double Get_L() const;
     double Get_S() const;
     double Get_R(double) const;
-    double Ger_rad() const;
+    double Get_rad() const;
     double Get_x(double) const;
     double Get_y(double) const;
     std::string Get_eq() const;
 };
-
