906319dc78d4d89cd6f901d85bdc7f561a426c27
ic_t <40525571+iconiq@users.noreply.github.com>
2020-10-01 15:12:04 +0300

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -6,10 +6,11 @@ void Menu() {
         "1. Print the length of the curve\n"
         "2. Print the radius of curvature\n"
         "3. Print the x and y values\n"
-        "4. Print the equation of the curve\n"
-        "5. Print the radius of curve\n"
-        "6. Change the radius of curve\n"
-        "7. Exit\n";
+        "4. Print area bounded curve\n"
+        "5. Print the equation of the curve\n"
+        "6. Print the radius of curve\n"
+        "7. Change the radius of curve\n"
+        "8. Exit\n";
 }
 
 int main() {
@@ -29,7 +30,7 @@ int main() {
     }
     std::cout << "Functions:\n";
     Menu();
-    while (c != 7) {
+    while (c != 8) {
         std::cout << "Enter the command number: ";
         std::cin >> c;
         switch (c) {
@@ -57,16 +58,20 @@ int main() {
             break;
         }
         case 4: {
+            double s = n.Get_S();
+            std::cout << "Area: " << s << "\n";
+        }
+        case 5: {
             std::string str = n.Get_eq();
             std::cout << str << "\n";
             break;
         }
-        case 5: {
+        case 6: {
             double f = n.Ger_rad();
             std::cout << "Radius: " << f << "\n";
             break;
         }
-        case 6: {
+        case 7: {
             double rad;
             std::cout << "Enter the new radius: ";
             std::cin >> rad;
@@ -75,4 +80,3 @@ int main() {
         }
     }
 }
-
