7f51061ae511ea19bbd189ea160a7926ed4b7bcb
ic_t <40525571+iconiq@users.noreply.github.com>
2020-11-09 09:56:27 +0300

    Update Scheme.cpp

--- lab3/B/Scheme.cpp
+++ lab3/B/Scheme.cpp
@@ -2,7 +2,7 @@
 
 Scheme::Scheme(int in, int ou) {
     if (in + ou > MAX_SIZE)
-        throw std::exception("Превышено максимальное количество клемм");
+        throw std::exception("РџСЂРµРІС‹С€РµРЅРѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РєР»РµРјРј");
     amount = in + ou;
     for (int i = 0; i < in; i++) {
         arr[i].number = i + 1;
@@ -18,8 +18,10 @@ Scheme::Scheme(int in, int ou) {
 }
 
 Scheme::Scheme(const struct clem* a, int size) {
+    if (a == nullptr)
+        throw std::exception("РњР°СЃСЃРёРІ РїСѓСЃС‚РѕР№");
     if (size > MAX_SIZE)
-        throw std::exception("Превышено максимальное количество клемм");
+        throw std::exception("РџСЂРµРІС‹С€РµРЅРѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ РєР»РµРјРј");
     for (int i = 0; i < size; i++) {
         arr[i] = a[i];
     }
@@ -28,7 +30,21 @@ Scheme::Scheme(const struct clem* a, int size) {
 
 std::istream& operator>>(std::istream& s, Scheme& scheme) {
     for (int i = 0; i < scheme.amount; i++) {
-        s >> scheme.arr[i].signal;
+        struct clem d;
+        s >> d.number >> d.type >> d.count >> d.signal;
+        if (d.type == 0 && d.count > 1 || d.type == 1 && d.count > 3) {
+            s.setstate(std::ios::failbit);
+            return s;
+        }
+        if (d.signal != 'x' && d.signal != '0' && d.signal != '1') {
+            s.setstate(std::ios::failbit);
+            return s;
+        }
+        if (d.type != 0 && d.type != 1) {
+            s.setstate(std::ios::failbit);
+            return s;
+        }
+        scheme.arr[i] = d;
     }
     return s;
 }
@@ -36,8 +52,8 @@ std::istream& operator>>(std::istream& s, Scheme& scheme) {
 std::ostream& operator<<(std::ostream& s, const Scheme& scheme) {   
     for (int i = 0; i < scheme.amount; i++) {
         std::string type, state;
-        type = (scheme.arr[i].type == 0) ? "входная" : "выходная";
-        s << "Номер: " << scheme.arr[i].number << "; Тип: " << type << "; Количество соединений: " << scheme.arr[i].count << "; Тип сигнала: " << scheme.arr[i].signal << "\n";
+        type = (scheme.arr[i].type == 0) ? "РІС…РѕРґРЅР°СЏ" : "РІС‹С…РѕРґРЅР°СЏ";
+        s << "РќРѕРјРµСЂ: " << scheme.arr[i].number << "; РўРёРї: " << type << "; РљРѕР»РёС‡РµСЃС‚РІРѕ СЃРѕРµРґРёРЅРµРЅРёР№: " << scheme.arr[i].count << "; РўРёРї СЃРёРіРЅР°Р»Р°: " << scheme.arr[i].signal << "\n";
     }
     return s;
 }
@@ -47,25 +63,41 @@ void Scheme::operator()(int num, int c) {
     if (i > -1)
         arr[i].signal = c;
     else
-        throw std::exception("Нет клеммы с таким номером");
+        throw std::exception("РќРµС‚ РєР»РµРјРјС‹ СЃ С‚Р°РєРёРј РЅРѕРјРµСЂРѕРј");
 }
 
-char Scheme::operator[](int num) {
+char Scheme::operator[](int num) const {
     int i = find(num);
     if (i > -1)
         return arr[i].signal;
     else
-        throw std::exception("Нет клеммы с таким номером");
+        throw std::exception("РќРµС‚ РєР»РµРјРјС‹ СЃ С‚Р°РєРёРј РЅРѕРјРµСЂРѕРј");
 }
 
-void Scheme::operator+=(struct clem c) {
-    if(amount == MAX_SIZE)
-        throw std::overflow_error("Достигнуто максимальное количество элементов");
-    arr[amount] = c;
-    amount++;
+Scheme& Scheme::operator+=(const Scheme& scheme) {
+    if(amount + scheme.amount > MAX_SIZE)
+        throw std::overflow_error("Р”РѕСЃС‚РёРіРЅСѓС‚Рѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ");
+    for (int i = 0; i < scheme.amount; i++) {
+        amount++;
+        arr[amount - 1] = scheme.arr[i];
+    }
 }
 
-int Scheme::find(int num) {
+const Scheme Scheme::operator+(const Scheme& scheme) {
+    Scheme s;
+    s.amount = amount + scheme.amount;
+    if (s.amount > MAX_SIZE)
+        throw std::exception("РљРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ Р±РѕР»СЊС€Рµ РґРѕРїСѓСЃС‚РёРјРѕРіРѕ");
+    for (int i = 0; i < amount; i++) {
+        s.arr[i] = arr[i];
+    }
+    for (int i = 0; i < scheme.amount; i++) {
+        s.arr[amount + i] = scheme.arr[i];
+    }
+    return s;
+}
+
+int Scheme::find(int num) const {
     for (int i = 0; i < amount; i++) {
         if (arr[i].number == num)
             return i;
@@ -77,26 +109,25 @@ void Scheme::add_clem_connection(int num, int count) {
     int i = find(num);
     if (i > -1) {
         if ((arr[i].type == 0 && count > 1) or (arr[i].type == 1 && count > 3)) {
-            throw std::exception("Превышено максимальное число соединений для клеммы");
+            throw std::exception("РџСЂРµРІС‹С€РµРЅРѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ С‡РёСЃР»Рѕ СЃРѕРµРґРёРЅРµРЅРёР№ РґР»СЏ РєР»РµРјРјС‹");
         }
         arr[i].count = count;
     }
     else
-        throw std::exception("Нет клеммы с таким номером");
+        throw std::exception("РќРµС‚ РєР»РµРјРјС‹ СЃ С‚Р°РєРёРј РЅРѕРјРµСЂРѕРј");
 }
 
 void Scheme::reduce_clem_connection(int num, int count) {
     int i = find(num);
     if (i > -1) {
         if ((arr[i].type == 0 && count > 1) or (arr[i].type == 1 && count > 3)) {
-            throw std::exception("Превышено максимальное число соединений для клеммы");
+            throw std::exception("РџСЂРµРІС‹С€РµРЅРѕ РјР°РєСЃРёРјР°Р»СЊРЅРѕРµ С‡РёСЃР»Рѕ СЃРѕРµРґРёРЅРµРЅРёР№ РґР»СЏ РєР»РµРјРјС‹");
         }
-        if ((arr[i].type == 0 && count > arr[i].count) or (arr[i].type == 1 && count > arr[i].count))
-        {
-            throw std::exception("Новое количество больше старого");
+        if (count > arr[i].count) {
+            throw std::exception("РќРѕРІРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ Р±РѕР»СЊС€Рµ СЃС‚Р°СЂРѕРіРѕ");
         }
         arr[i].count = count;
     }
     else
-        throw std::exception("Нет клеммы с таким номером");
+        throw std::exception("РќРµС‚ РєР»РµРјРјС‹ СЃ С‚Р°РєРёРј РЅРѕРјРµСЂРѕРј");
 }
