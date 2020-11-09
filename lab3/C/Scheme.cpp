4c5240c307074256e523d92ca7cf135207ddfe91
ic_t <40525571+iconiq@users.noreply.github.com>
2020-11-09 11:52:57 +0300

    Update Scheme.cpp

--- lab3/C/Scheme.cpp
+++ lab3/C/Scheme.cpp
@@ -4,12 +4,10 @@ Scheme::Scheme(int in, int out) {
     amount = in + out;
     arr = new struct clem[amount];
     for (int i = 0; i < in; i++) {
-        arr[i].number = i + 1;
         arr[i].type = input;
         arr[i].count = 0;
         arr[i].signal = 'x';
     } for (int i = in; i < amount; i++) {
-        arr[i].number = i + 1;
         arr[i].type = output;
         arr[i].count = 0;
         arr[i].signal = 'x';
@@ -27,17 +25,24 @@ Scheme::Scheme(const struct clem* a, int size) {
 
 Scheme::Scheme(const Scheme& scheme) {
     amount = scheme.amount;
-    arr = new struct clem[amount];
-    for (int i = 0; i < amount; i++)
-        arr[i] = scheme.arr[i];
+    if (amount == 0) {
+        arr = nullptr;
+    }
+    else {
+        arr = new struct clem[amount];
+        for (int i = 0; i < amount; i++)
+            arr[i] = scheme.arr[i];
+    }
+}
+
+Scheme::Scheme(struct clem s) {
+    amount = 1;
+    arr[0] = s;
 }
 
 Scheme::Scheme(Scheme&& scheme) {
     amount = scheme.amount;
-    arr = new struct clem[amount];
-    for (int i = 0; i < amount; i++)
-        arr[i] = scheme.arr[i];
-    delete[] scheme.arr;
+    arr = scheme.arr;
     scheme.amount = 0;
     scheme.arr = nullptr;
 }
@@ -62,45 +67,41 @@ Scheme& Scheme::operator=(const Scheme& scheme) {
 
 Scheme& Scheme::operator=(Scheme&& scheme) {
     if(this != &scheme) {
-        struct clem* tmp;
-        try {
-            tmp = new struct clem[scheme.amount];
-        }
-        catch (const std::bad_alloc&) {
-            throw "Произошла ошибка при выделении памяти\n";
-        }
         delete[] arr;
         amount = scheme.amount;
-        for (int i = 0; i < amount; i++)
-            tmp[i] = scheme.arr[i];
-        arr = tmp;
+        arr = scheme.arr;
         scheme.amount = 0;
-        delete[] scheme.arr;
         scheme.arr = nullptr;
     }
     return *this;
 }
 
 std::istream& operator>>(std::istream& s, Scheme& scheme) {
+    struct clem* tmp = new struct clem[scheme.amount];
     for (int i = 0; i < scheme.amount; i++) {
         struct clem d;
         int a;
-        s >> d.number >> a >> d.count >> d.signal;
+        s >> a >> d.count >> d.signal;
         d.type = static_cast<Type>(a);
         if (d.type == input && d.count > 1 || d.type == output && d.count > 3) {
             s.setstate(std::ios::failbit);
+            delete[] tmp;
             return s;
         }
         if (d.signal != 'x' && d.signal != '0' && d.signal != '1') {
             s.setstate(std::ios::failbit);
+            delete[] tmp;
             return s;
         }
         if (d.type != input && d.type != output) {
             s.setstate(std::ios::failbit);
+            delete[] tmp;
             return s;
         }
-        scheme.arr[i] = d;
+        tmp[i] = d;
     }
+    delete[] scheme.arr;
+    scheme.arr = tmp;
     return s;
 }
 
@@ -108,23 +109,21 @@ std::ostream& operator<<(std::ostream& s, const Scheme& scheme) {
     for (int i = 0; i < scheme.amount; i++) {
         std::string type;
         type = (scheme.arr[i].type == input) ? "входная" : "выходная";
-        s << "Номер: " << scheme.arr[i].number << "; Тип: " << type << "; Количество соединений: " << scheme.arr[i].count << "; Тип сигнала: " << scheme.arr[i].signal << "\n";
+        s << "Номер: " << i+1 << "; Тип: " << type << "; Количество соединений: " << scheme.arr[i].count << "; Тип сигнала: " << scheme.arr[i].signal << "\n";
     }
     return s;
 }
 
 void Scheme::operator()(int num, int c) {
-    int i = find(num);
-    if (i > -1)
-        arr[i].signal = c;
+    if (num <= amount)
+        arr[num-1].signal = c;
     else
         throw std::exception("Нет клеммы с таким номером");
 }
 
 char Scheme::operator[](int num) const {
-    int i = find(num);
-    if (i > -1)
-        return arr[i].signal;
+    if (num <=  amount)
+        return arr[num-1].signal;
     else
         throw std::exception("Нет клеммы с таким номером");
 }
@@ -143,57 +142,52 @@ Scheme& Scheme::operator+=(const Scheme& scheme) {
     for (int i = amount; i < scheme.amount; i++) {
         ax[i] = scheme.arr[i];
     }
+    delete[] arr;
     arr = ax;
     amount += scheme.amount;
 }   
 
-const Scheme Scheme::operator+(const Scheme& scheme) const {
+const Scheme operator+(const Scheme& scheme1, const Scheme& scheme2) {
     Scheme s;
-    s.amount = amount + scheme.amount;
-    try {
-        s.arr = new struct clem[amount + scheme.amount];
-    } catch (const std::bad_alloc&) {
-        throw "Произошла ошибка при выделении памяти\n";
+    s.amount = scheme1.amount + scheme2.amount;
+    s.arr = new struct clem[s.amount];
+    for (int i = 0; i < scheme1.amount; i++) {
+        s.arr[i] = scheme1.arr[i];
     }
-    for (int i = 0; i < amount; i++) {
-        s.arr[i] = arr[i];
-    }
-    for (int i = 0; i < scheme.amount; i++) {
-        s.arr[amount + i] = scheme.arr[i];
+    for (int i = 0; i < scheme2.amount; i++) {
+        s.arr[scheme1.amount + i] = scheme2.arr[i];
     }
     return s;
 }
 
-int Scheme::find(int num) const {
-    for (int i = 0; i < amount; i++) {
-        if (arr[i].number == num)
-            return i;
-    }
-    return -1;
-}
+//int Scheme::find(int num) const {
+//    for (int i = 0; i < amount; i++) {
+//        if (arr[i].number == num)
+//            return i;
+//    }
+//    return -1;
+//}
 
 void Scheme::add_clem_connection(int num, int count) {
-    int i = find(num);
-    if (i > -1) {
-        if ((arr[i].type == 0 && count > 1) or (arr[i].type == 1 && count > 3)) {
+    if (num <= amount) {
+        if ((arr[num-1].type == 0 && count > 1) or (arr[num-1].type == 1 && count > 3)) {
             throw std::exception("Превышено максимальное число соединений для клеммы");
         }
-        arr[i].count = count;
+        arr[num-1].count = count;
     }
     else
         throw std::exception("Нет клеммы с таким номером");
 }
 
 void Scheme::reduce_clem_connection(int num, int count) {
-    int i = find(num);
-    if (i > -1) {
-        if ((arr[i].type == input && count > 1) or (arr[i].type == output && count > 3)) {
+    if (num <= amount) {
+        if ((arr[num-1].type == input && count > 1) or (arr[num-1].type == output && count > 3)) {
             throw std::exception("Превышено максимальное число соединений для клеммы");
         }
-        if (count > arr[i].count) {
+        if (count > arr[num-1].count) {
             throw std::exception("Новое количество больше старого");
         }
-        arr[i].count = count;
+        arr[num-1].count = count;
     }
     else
         throw std::exception("Нет клеммы с таким номером");
