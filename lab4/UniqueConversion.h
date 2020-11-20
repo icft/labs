9b3005287015a874c5175389a9cb8009cd3bf266
ic_t <40525571+iconiq@users.noreply.github.com>
2020-11-20 18:49:26 +0300

    Update UniqueConversion.h

--- lab4/UniqueConversion.h
+++ lab4/UniqueConversion.h
@@ -1,16 +1,16 @@
 #include <memory>
 
-template<typename Derived, typename Base, typename Del>
-std::unique_ptr<Derived, Del> static_unique_ptr_cast(std::unique_ptr<Base, Del>&& p) {
+template<typename Derived, typename Base>
+std::unique_ptr<Derived> static_unique_ptr_cast(std::unique_ptr<Base>&& p) {
     auto d = static_cast<Derived*>(p.release());
-    return std::unique_ptr<Derived, Del>(d, std::move(p.get_deleter()));
+    return std::unique_ptr<Derived>(d, std::move(p.get_deleter()));
 }
 
-template<typename Derived, typename Base, typename Del>
-std::unique_ptr<Derived, Del> dynamic_unique_ptr_cast(std::unique_ptr<Base, Del>&& p) {
+template<typename Derived, typename Base>
+std::unique_ptr<Derived> dynamic_unique_ptr_cast(std::unique_ptr<Base>&& p) {
     if (Derived* result = dynamic_cast<Derived*>(p.get())) {
         p.release();
-        return std::unique_ptr<Derived, Del>(result, std::move(p.get_deleter()));
+        return std::unique_ptr<Derived>(result, std::move(p.get_deleter()));
     }
-    return std::unique_ptr<Derived, Del>(nullptr, p.get_deleter());
+    return std::unique_ptr<Derived>(nullptr, p.get_deleter());
 }
