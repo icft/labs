6bc826cc2d2f4d97b617a368b7c1594ae3c832df
ic_t <40525571+iconiq@users.noreply.github.com>
2020-11-22 21:43:12 +0300

    Update CryptFile.h

--- lab4/CryptFile.h
+++ lab4/CryptFile.h
@@ -1,27 +1,16 @@
 #pragma once
 
 #include <ctime>
+#include <list>
 #include <string>
 #include "File.h"
-#include "List.h"
-#include "Node.h>
 
-class crypt_file : public node {
+class crypt_file: public ffile {
 private:
-	std::unique_ptr<FILE> data;
-	tm create;
-	tm last_modify;
-	std::string owner;
-	list<std::pair<std::string, access>> rights;
-	size_t size;
 public:
+	crypt_file();
 	~crypt_file();
-	friend std::istream& operator>> (std::istream&, crypt_file&);
-	void edit();
-	friend std::ostream& operator<< (std::ostream&, const crypt_file&);
-	void operator+= (std::string);
-	void edit_access();
 	ffile& decrypt();
-	void rename();
 };
+
 
