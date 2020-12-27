11e86f6d24860cb4e9e5aaa939eb55c5056d60f5
ic_t <40525571+iconiq@users.noreply.github.com>
2020-12-27 23:22:06 +0300

    Update Dir.h

--- lab4/Dir.h
+++ lab4/Dir.h
@@ -5,19 +5,57 @@
 #include "Map.h"
 #include <iostream>
 
+/*!
+*       \brief Данный класс, описывает основные функции и переменные для работы с директориями
+*/
 class Dir : public File {
 public:
-	Map<std::string, File*> elems;
+    std::map<std::string, File*> elems;
 
-	Dir() = default;
-	~Dir() = default;
-	void print() {
-		Map<std::string, File*>::ConstIterator it = elems.cbegin();
-		for (; it != elems.cend(); it++) {
-			if (data.empty())
-				std::cout << name << "--- dir\n";
-			else
-				std::cout << name << "--- file\n";
-		}
-	}
+    Dir(std::string dirname, std::string own) {
+        filename = dirname;
+        this_is_dir = true;
+        owner = own;
+        time_t now = time(0);
+        create_time = localtime(&now);
+        last_modify = localtime(&now);
+        user_rights[own] = { true, true, true };
+        if (own != "admin")
+            user_rights["admin"] = { true, true, true };
+    }
+    ~Dir() = default;
+    /*!
+    Добавить файл
+    \param[in] f указатель на файл
+    \param[in] owner имя пользователя
+    */
+    bool add_file(File* f, std::string owner) {
+        auto it = user_rights.find(owner);
+        if (it == user_rights.end()) {
+            return false;
+        }
+        auto ar = it->second;
+        if (ar.can_execute_or_move && ar.can_write) {
+            elems[f->filename] = f;
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    /*!
+    Вывести директорию*/
+    void print() {
+        //		std::map<std::string, File*>::iterator it = elems.begin();
+        for (auto it = elems.begin(); it != elems.end(); it++) {
+            if (it->second->is_dir())
+                std::cout << it->first << " --- dir\n";
+            else
+                std::cout << it->first << " --- file\n";
+        }
+    }
+    //    void create(std::string filename){
+    //        this->filename = filename;
+    //    }
 };
