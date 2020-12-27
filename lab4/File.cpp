3d0278674d01b08fb837ae07bf568529ec1e00f5
ic_t <40525571+iconiq@users.noreply.github.com>
2020-12-27 23:21:35 +0300

    Update File.cpp

--- lab4/File.cpp
+++ lab4/File.cpp
@@ -1,74 +1,83 @@
 #include "File.h"
 #include <ctime>
-#include <exception>
-#include <list>
-#include <string>
-#include <sstream>
-void File::create(std::string nm, std::string stream, std::string s, std::string own) {
-	name = nm;
-	time_t now = time(0);
-	create_time = localtime(&now);
-	last_modify = localtime(&now);
-	owner = own;
-	Map<std::string, std::string>::Iterator it = data.find(stream);
-	if (it != data.end())
-		throw std::exception("Stream exist");
-	std::pair<std::string, std::string> d;
-	d.first = stream;
-	d.second = s;
-	data.add(d);
-	rights[own] = { true, true, true };
-	size[stream] = s.length();
+//#include <exception>
+//#include <stdexcept>
+//#include <list>
+//#include <string>
+//#include <sstream>
+
+
+bool File::create(std::string nm, std::string stream, char* content, int content_size, std::string own) {
+    filename = nm;
+    time_t now = time(0);
+    this->create_time = localtime(&now);
+    this->last_modify = localtime(&now);
+    this->owner = own;
+    //    Map<std::string, std::string>::Iterator it = data.find(stream);
+    if (stream != "MAIN" && data.is_empty())
+        return false;
+    auto it = data.find(stream);
+    if (it != data.end())
+        throw std::runtime_error("Stream exist");
+    //	std::pair<std::string, std::string> d;
+    //	d.first = stream;
+    //    d.second = content;
+    //    data.add(d);
+    data[stream] = new char[content_size];
+    sz[stream] = content_size;
+    memcpy(data[stream], content, content_size);
+    user_rights["admin"] = { true, true, true };
+    user_rights[own] = { true, true, true };
+    return true;
 }
 
-void File::edit(std::string stream, std::string s) {
-	Map<std::string, std::string>::Iterator it = data.find(stream);
-	if (it == data.end())
-		throw std::exception("Stream doesn't exist");
-	it.edit_value(s);
-	size[stream] = s.length();
+void File::edit(std::string stream, char* content, int content_size, std::string user) {
+    //    Map<std::string, std::string>::Iterator it = data.find(stream);
+    auto it = data.find(stream);
+    if (it == data.end())
+        throw std::runtime_error("Stream doesn't exist");
+    //    it.edit_value(content);
+    if (user_rights.count(user) == 1) {
+        it->second = new char[content_size];
+        memcpy(it->second, content, content_size);
+    }
+    sz[stream] = content_size;
 }
 
-std::string File::get_data(std::string stream) {
-	Map<std::string, std::string>::Iterator it = data.find(stream);
-	if (it == data.end())
-		throw std::exception("Stream doesn't exist");
-	return it.get_value();
+char* File::get_data(std::string stream, std::string user, int* out_len) {
+    //    Map<std::string, std::string>::Iterator it = data.find(stream);
+    auto it = data.find(stream);
+    if (it == data.end())
+        throw std::runtime_error("Stream doesn't exist");
+    if (user_rights.count(user) == 1) {
+        *out_len = sz[stream];
+        return it->second;
+        //    return it.get_value();
+    }
+    else {
+        *out_len = -1;
+        return nullptr;
+    }
 }
 
-void File::add_access(std::string s, access_rights ac) {
-	rights[s] = ac;
+
+void File::set_access(std::string user_name, access_rights ac) {
+    user_rights[user_name] = ac;
 }
 
-void File::edit_access(std::string s, access_rights ac) {
-	std::map<std::string, access_rights>::iterator it = rights.find(s);
-	if (it == rights.end())
-		throw std::exception("User doesn't exist");
-	rights[s] = ac;
-}
-
-std::string File::get_sizes() const {
-	std::string s="";
-	std::map<std::string, int>::const_iterator it = size.cbegin();
-	for (; it != size.cend(); it++) {
-		std::stringstream ss;
-		ss << it->second;
-		std::string p;
-		ss >> p;
-		s += "Stream-" + it->first + "; " + "Size-" + p;
-		s += "\n";
-	}
-	return s;
-}
 
 std::ostream& operator<<(std::ostream& s, const File& f) {
-	s << "Name: " << f.name << "\n";
-	s << "Time of creation: " << f.create_time->tm_hour << ":" << f.create_time->tm_min << "\n";
-	s << "Date of creation: " << f.create_time->tm_mday << "." << f.create_time->tm_mon << "." << f.create_time->tm_year << "\n";
-	s << "Time of last modification: " << f.last_modify->tm_hour << ":" << f.last_modify->tm_min << "\n";
-	s << "Date of last modification: " << f.last_modify->tm_mday << "." << f.last_modify->tm_mon << "." << f.last_modify->tm_year << "\n";
-	s << f.get_sizes();
-	s << "Owner: " << f.owner;
+    s << "Time of creation: " << f.create_time->tm_hour << ":" << f.create_time->tm_min << "\n";
+    s << "Date of creation: " << f.create_time->tm_mday << "." << f.create_time->tm_mon << "." << f.create_time->tm_year << "\n";
+    s << "Time of last modification: " << f.last_modify->tm_hour << ":" << f.last_modify->tm_min << "\n";
+    s << "Date of last modification: " << f.last_modify->tm_mday << "." << f.last_modify->tm_mon << "." << f.last_modify->tm_year << "\n";
+    for (auto it = f.sz.begin(); it != f.sz.end(); it++) {
+        s << "Stream '" << it->first << "' size " << it->second;
+    }
+    //    s << f.get_sizes_for_print();
+    s << "Owner: " << f.owner;
+    return s;
 }
+
 
 
