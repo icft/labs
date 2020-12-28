ca433e9b9286e06f099725a7d5e3c3b783f73de3
ic_t <40525571+iconiq@users.noreply.github.com>
2020-12-28 17:09:48 +0300

    Update Position.h

--- lab5/Position.h
+++ lab5/Position.h
@@ -3,15 +3,15 @@
 
 class Position {
 private:
-	int markers;
-	std::vector<int> out;
+	size_t markers;
+	std::vector<size_t> out;
 public:
-	Position() : markers{} {}
-	Position(int transitions) : markers{}, out(transitions) {}
-	Position(std::vector<int>&& out) : markers{}, out(std::move(out)) {}
-	auto get_num_of_markers() const { return markers; }
+	Position() : markers{} {} 
+	Position(const size_t transitions) : markers{}, out(transitions) {}
+	Position(std::vector<size_t>&& out) : markers{}, out(std::move(out)) {}
+	constexpr auto get_num() const { return markers; }
 	const auto& get_out() const { return out; }
-	void add_markers(const int num) { markers += num; }
-	void remove_markers(const int num) { markers -= num; }
-	void update_transition(const int idx, const int num) { out.at(idx) += num; }
+	void add(const size_t num) { markers += num; }
+	void remove(const size_t num) { markers -= num; }
+	void update(const size_t ind, const size_t num) { out.at(ind) += num; }
 };
