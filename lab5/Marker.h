73ad7640b0f36bce1f2d8afb0e2465752da2aa58
ic_t <40525571+iconiq@users.noreply.github.com>
2020-12-28 17:09:26 +0300

    Update Marker.h

--- lab5/Marker.h
+++ lab5/Marker.h
@@ -2,27 +2,27 @@
 #include <vector>
 #include <chrono>
 
-class Marker {
+class Marker{
 private:
-	unsigned long long ID = 0LLU;
 	bool blocked;
-	int id;
-	int pos;
-	std::vector<int> transitions;
+	size_t id;
+	size_t position;
+	std::vector<size_t> transitions;
 	std::chrono::time_point<std::chrono::system_clock> time;
 	std::chrono::milliseconds duration;
+	inline static auto ID{ 0LLU };
 public:
-	Marker(const int position) : blocked{}, id(Marker::ID++), pos(position), duration{} {}
-	int get_id() const { return id; }
-	int get_position() const { return pos; }
+	Marker(const size_t pos) : blocked{}, id(Marker::ID++), position(pos), duration{} {} 
+	constexpr auto get_id() const { return id; } 
+	constexpr auto get_position() const { return position; } 
 	const auto& get_transitions() const { return transitions; }
-	auto get_time() const  { return time; }
-	auto get_duration() const { return duration; }
-	bool is_blocked() const { return blocked; }
+	constexpr auto get_time() const  { return time; }
+	constexpr auto get_duration() const { return duration; }
+	constexpr bool is_blocked() const { return blocked; }
 	void set_blocked(const bool value) { blocked = value; }
-	void set_position(const int position) { pos = position; }
-	void set_time(const std::chrono::time_point < std::chrono::system_clock>& t) { time = t; }
-	void set_duration(const std::chrono::milliseconds& d) { duration = d; }
-	void add_transition(const int idx) { transitions.emplace_back(idx); }
+	void set_position(const size_t pos) { position = pos; }
+	void set_timepoint(const std::chrono::time_point<std::chrono::system_clock>& t) { time = t; }
+	void set_duration(const std::chrono::milliseconds& dur) { duration = dur; }
+	void add_transition(const size_t idx) { transitions.emplace_back(idx); }
 	void clear_transitions() { transitions.clear(); };
 };
