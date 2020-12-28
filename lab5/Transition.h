cf60f19faa8300d96d347341586a499256011e24
ic_t <40525571+iconiq@users.noreply.github.com>
2020-12-28 17:10:06 +0300

    Update Transition.h

--- lab5/Transition.h
+++ lab5/Transition.h
@@ -3,15 +3,15 @@
 
 class Transition {
 private:
-	std::vector<int> inp;
-	std::vector<int> out;
-	void update(std::vector<int>& vec, const int idx, const int num) { vec[idx] += num; }
+	std::vector<size_t> inp;
+	std::vector<size_t> out;
+	void update(std::vector<size_t>& vec, const size_t ind, const size_t num) { vec.at(ind) += num; }
 public:
 	Transition() = default;
-	Transition(int positions) : inp(positions), out(positions) {}
-	Transition(std::vector<int>&& inputs, std::vector<int>&& outputs) : inp(std::move(inputs)), out(std::move(outputs)) {}
-	const auto& get_inputs() const { return inp; }
-	const auto& get_outputs() const { return out; }
-	void update_input(const int idx, const int num) { update(inp, idx, num); }
-	void update_output(const int idx, const int num) { update(out, idx, num); }
+	Transition(const std::size_t positions) : inp(positions), out(positions) {}
+	Transition(std::vector<size_t>&& inputs, std::vector<size_t>&& outputs) : inp(std::move(inputs)), out(std::move(outputs)) {}
+	const auto& get_inp() const noexcept { return inp; }
+	const auto& get_out() const noexcept { return out; }
+	void update_inp(const size_t ind, const size_t num) { update(inp, ind, num); }
+	void update_out(const size_t ind, const size_t num) { update(out, ind, num); }
 };
